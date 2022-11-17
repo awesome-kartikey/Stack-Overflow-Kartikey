efc05c9d70d19ddf8fe05606c73ededd67a9c13c
Kartikey Kumar <46820893+awesome-kartikey@users.noreply.github.com>
2022-11-17 16:04:40 +0530

    Auth Page Complete

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import {Routes, Route} from 'react-router-dom'
 
-import Home from './pages/Home/Home'
-import Auth from './pages/Auth/Auth'
+import Home from './Pages/Home/Home'
+import Auth from './Pages/Auth/Auth'
 
 const AllRoutes = () => {
   return (
--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,12 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Auth.css";
+import icon from "../../Assets/icon.png";
+import AboutAuth from "./AboutAuth";
+
 
 const Auth = () => {
+  const [isSignup, setIsSignup] = useState(false);
+
+  const handleSwitch = () => {
+    setIsSignup(!isSignup);
+  };
+
   return (
     <div>
-      <h1>Auth</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur, corrupti accusamus. Ab, maxime esse. Qui ex vel, nam in delectus magnam nobis odit error dolorum quia! Maiores aliquid exercitationem harum.</p>
-    </div>
-  )
-}
+      <section className="auth-section">
+        {isSignup && <AboutAuth />}
+        <div className="aut-container-2">
+          {!isSignup && (
+            <img src={icon} alt="stack overflow" classname="login-logo" />
+          )}
+          <form>
+              {
+                isSignup && (
+                    <label htmlFor="">
+                      <h4>Enter Name</h4>
+                      <input type="text" id='name' name='name'/>
+                    </label>
+                )
+              }
 
-export default Auth
+
+            <label htmlFor="email">
+              <h4>Email</h4>
+              <input type="email" name="email" id="email" />
+            </label>
+            <label htmlFor="password">
+              <div style={{display:"flex", justifyContent:"space-between"}}>
+                <h4>Password</h4>
+                {!isSignup && <p style={{color:"#666767", fonSize:"13px"}}>Forgot Password?</p>}
+              </div>
+              <input type="password" name="password" id="password" />
+              {isSignup && <p style={{color:"#666767", fontSize:"13px"}}>Passwords must contain at least eight <br/>characters, including at least 1 letter <br/>and 1 number.</p>}
+            </label>
+            {
+              isSignup && (
+                <label htmlFor="check">
+                    <input type="checkbox" id="check"/>
+                    <p style={{fontSize:"13px"}}>Opt-in to receive occasional product<br/> updates, user research invitations,<br/> company announcements, and digests.</p>
+                </label>
+              )
+            }
+            <button type="submit" className="auth-btn">
+              {isSignup ? "Sign Up" : "Log in"}{" "}
+            </button>
+            {
+              isSignup && (
+                <p style={{color: "#666767", fontSize:"13px"}}>
+                  By clicking “Sign up”, you agree to our 
+                  <span style={{color: "#007ac6"}}> terms of <br/>services</span> , 
+                  <span style={{color: "#007ac6"}}>privacy policy</span> and 
+                  <span style={{color: "#007ac6"}}> cookie policy</span>
+                </p>
+              )
+            }
+          </form>
+          <p>
+            {isSignup ? "Already have an account?" : "Don't have an account?"}
+            <button
+              type="button"
+              className="handle-switch-btn"
+              onClick={handleSwitch}
+            >
+              {isSignup ? "Log in" : "Sign Up"}
+            </button>
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Auth;
