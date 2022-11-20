53f6892ffd1fc5f0c3ec9f3e68bbf221fdbd273d
Kartikey Kumar <46820893+awesome-kartikey@users.noreply.github.com>
2022-11-20 22:14:15 +0530

    Stack Overflow Ask Question Backend Complete

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import userRoutes from './routes/users.js'
+import questionRoutes from './routes/Questions.js'
 
 const app = express();
 app.use(express.json({limit: "30mb", extended: true}));
@@ -13,6 +14,7 @@ app.get('/',(req, res) => {
 })
 
 app.use('/user', userRoutes)
+app.use('/questions', questionRoutes)
 
 const PORT = process.env.PORT || 5000
 
---
+++ server/routes/Questions.js
@@ -0,0 +1,8 @@
+import express from "express";
+import {AskQuestion} from "../controllers/Questions.js"
+
+const router = express.Router()
+
+router.post('/Ask', AskQuestion)
+
+export default router
